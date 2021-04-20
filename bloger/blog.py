766d4e6200b9a8c23a9814fb0e2e9763326ba105
nyambayar <nyambayar@steppelink.mn>
2021-04-20 16:15:49 +0800

    moved name to bloger

--- bloger/blog.py
+++ bloger/blog.py
@@ -3,8 +3,8 @@ from flask import (
 )
 from werkzeug.exceptions import abort
 
-from flaskr.auth import login_required
-from flaskr.db import get_db
+from bloger.auth import login_required
+from bloger.db import get_db
 
 bp = Blueprint('blog', __name__)
 
